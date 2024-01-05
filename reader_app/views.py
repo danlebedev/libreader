6ed0c0f5ef4462116f1548d767f9be9ed402bccb
danlebedev <lebedev.py@gmail.com>
2024-01-05 15:36:23 +0300

    add in reader_app other page

--- reader_app/views.py
+++ reader_app/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.urls import reverse
 import os
 import json
 
@@ -11,6 +12,42 @@ with open(LIBRARY_STRUCTURE_TREE_JSON) as f:
     TREE_JSON = json.load(f)
 
 
+def sort_dict(dict_node, key=''):
+    sorted_dict = {}
+    for k, v in dict_node.items():
+        if isinstance(v, dict):
+            if k.isnumeric():
+                dct = dict_node[k]
+                dct["route"] = key + '/' + k
+                sorted_dict[k] = sort_dict(v, key + '/' + k)
+            else:
+                sorted_dict[k] = sort_dict(v, key)
+        else:
+            sorted_dict[k] = v
+    return sorted_dict
+
+NEW_DICT = sort_dict(TREE_JSON)
+
+
 def index(request):
-    context = {"tree_root": TREE_JSON["path"]}
+    context = {"tree_root": NEW_DICT["path"]}
     return render(request, 'reader_app/index.html', context)
+
+
+def other(request, path):
+    node = tree_processing(path)
+    if node.get("folder"):
+        with open((os.path.join(LIBRARY_ROOT["path"], node["folder"], 'document.xml'))) as f:
+            page = f.read()
+        return HttpResponse(page)
+    context = {"node": node["path"]}
+    return render(request, 'reader_app/other.html', context)
+
+
+def tree_processing(path):
+    path_keys = path.split('/')
+    node = TREE_JSON
+    for key in path_keys:
+        node = node["path"]
+        node = node[key]
+    return node
