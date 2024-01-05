cd27487dbf79b72c6e81f15cf437ee2bf5a6f9f2
danlebedev <lebedev.py@gmail.com>
2024-01-05 10:42:47 +0300

    add in reader_app display of library root

--- reader_app/views.py
+++ reader_app/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+import os
+import json
+
+
+LIBRARY_ROOT_JSON = os.path.join(os.path.abspath(os.curdir), 'reader_app', 'services', 'library_root.json')
+with open(LIBRARY_ROOT_JSON, 'r') as f:
+    LIBRARY_ROOT = json.load(f)
+LIBRARY_STRUCTURE_TREE_JSON = os.path.join(LIBRARY_ROOT["path"], "_structure", "tree.json")
+with open(LIBRARY_STRUCTURE_TREE_JSON) as f:
+    TREE_JSON = json.load(f)
+
 
 def index(request):
-    return render(request, 'reader_app/index.html')
+    context = {"tree_root": TREE_JSON["path"]}
+    return render(request, 'reader_app/index.html', context)
